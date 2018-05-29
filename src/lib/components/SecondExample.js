68743d898c11cae021ec72a2df075177c2c59f74
Stephen Pontes <stephen@masterclass.com>
2018-05-29 14:00:24 -0700

    Removing default styles, importing existing structure

--- src/lib/components/SecondExample.js
+++ src/lib/components/SecondExample.js
@@ -2,26 +2,18 @@ import React from 'react';
 import './SecondExample.css';
 
 const SecondExample = () => (
-  <div className="SecondExample">
-    <p className="SecondExample-text">
-      Based on Facebook's {'\u00A0'}
-      <a
-        className="SecondExample-link"
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://github.com/facebookincubator/create-react-app"
-      >
-        Create react app
-      </a>
-    </p>
-    <a
-      className="SecondExample-github-link"
-      target="_blank"
-      rel="noopener noreferrer"
-      href="https://github.com/UdiliaInc/create-react-library"
-    >
-      Documentation
-    </a>
+  <div className="container">
+    <div className="row">
+      <div className="col-sm">
+        One of three columns
+      </div>
+      <div className="col-sm">
+        One of three columns
+      </div>
+      <div className="col-sm">
+        One of three columns
+      </div>
+    </div>
   </div>
 );
 
